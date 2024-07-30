11b7f3f79519dcb74534edc3976575e56dab36c2
Minji Kim <mjkim103301@gmail.com>
2024-07-30 23:56:43 +0900

    Time: 0 ms (100%), Space: 42.7 MB (73.92%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -6,19 +6,13 @@ class Solution {
         while (left <= right) {
             int mid = (left + right) / 2;
             if (nums[mid] == target) {
-                answer = mid;
-                break;
+                return mid;
             } else if (nums[mid] > target) {
-                answer = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        if (nums[nums.length-1]<target) {
-            answer = nums.length;
-        }
-
-        return answer;
+        return left;
     }
 }

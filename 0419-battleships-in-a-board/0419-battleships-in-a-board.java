a10e608768f25b2d03d32b948d2c08944a7d2f21
Minji Kim <mjkim103301@gmail.com>
2024-09-11 05:04:52 +0900

    Time: 1 ms (93.4%), Space: 45.4 MB (41.83%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,5 +1,4 @@
 class Solution {
-    public boolean[][] visited;
     public int[][] move = {
             { 0, 1 },
             { 0, -1 },
@@ -8,11 +7,10 @@ class Solution {
     };
 
     public int countBattleships(char[][] board) {
-        visited = new boolean[board.length][board[0].length];
         int count = 0;
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[0].length; x++) {
-                if (!visited[y][x] && board[y][x] == 'X') {
+                if (board[y][x] == 'X') {
                     findSet(y, x, board);
                     count++;
                 }
@@ -22,16 +20,15 @@ class Solution {
     }
 
     public void findSet(int y, int x, char[][] board) {
-        visited[y][x] = true;
+        board[y][x] = '.';
         for (int i = 0; i < 4; i++) {
             int ny = y + move[i][0];
             int nx = x + move[i][1];
-            if (ny < 0 || nx < 0 || ny >= board.length || nx >= board[0].length || visited[ny][nx]
+            if (ny < 0 || nx < 0 || ny >= board.length || nx >= board[0].length
                     || board[ny][nx] != 'X') {
                 continue;
             }
             findSet(ny, nx, board);
         }
-
     }
 }

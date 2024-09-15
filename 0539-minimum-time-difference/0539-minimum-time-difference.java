2189008549efc0eddd31c30bf5dc7906dd78510e
Minji Kim <mjkim103301@gmail.com>
2024-09-15 16:26:26 +0900

    Time: 14 ms (22.97%), Space: 45.2 MB (71.45%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -9,21 +9,18 @@ class Solution {
         for (String time : timePoints) {
             int hour = Integer.parseInt(time.substring(0, 2));
             int minute = Integer.parseInt(time.substring(3));
-            int temp = hour * 60 + minute;
-            minutes.add(temp);
+            minutes.add(hour * 60 + minute);
         }
         return minutes;
     }
 
     public int minDifference(List<Integer> minutes) {
         Collections.sort(minutes);
-        int min = minutes.get(minutes.size()-1)-minutes.get(0);
-        min = Math.min(min, 1440-min);
+        int min = minutes.get(minutes.size() - 1) - minutes.get(0);
+        min = Math.min(min, 1440 - min);
         for (int i = 1; i < minutes.size(); i++) {
             int diff = minutes.get(i) - minutes.get(i - 1);
-            if (min > diff) {
-                min = diff;
-            }
+            min = Math.min(min, diff);
         }
         return min;
     }

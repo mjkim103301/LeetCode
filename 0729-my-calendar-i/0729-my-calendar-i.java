7928f050c82f976fa8ccfff1e9b704d24f9eae59
Minji Kim <mjkim103301@gmail.com>
2024-09-20 19:18:47 +0900

    Time: 78 ms (29.47%), Space: 45.5 MB (43.87%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,25 @@
+class MyCalendar {
+    List<int[]> events;
+
+    public MyCalendar() {
+        events = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] event : events) {
+            if (start >= event[1] || end <= event[0]) {
+                continue;
+            }
+            return false;
+        }
+        events.add(new int[] { start, end });
+        return true;
+
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
